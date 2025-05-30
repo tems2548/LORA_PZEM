9e43c1459e9ae8517942c39d5fe57ab8beaede77
TemZ <110783208+tems2548@users.noreply.github.com>
2025-05-30 13:32:37 +0700

    update data

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,14 @@
+// AS923 in thailand 920~925
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define Debug_MSG true
+#define Send_MSG true
+#define Read_MSG true 
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+ 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  
 }
