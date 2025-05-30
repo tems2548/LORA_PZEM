98dae237796c1f76722c3666f05b588f088fcc5b
TemZ <110783208+tems2548@users.noreply.github.com>
2025-05-31 00:08:17 +0700

    ...

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
 // AS923 in thailand 920~925
+
+// Hardwere heltec wifi lora 32 V3 
+// PZEM-oo4T Power sensor
+
 #include <Arduino.h>
 
 #define Debug_MSG true
 #define Send_MSG true
 #define Read_MSG true 
+
 
 void setup() {
  
